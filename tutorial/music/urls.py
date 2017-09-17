ce164dc6aff1b04ba93cbdb65579ad2336c51752
Alex <alejandrolachs@gmail.com>
2017-09-17 01:12:08 -0500

    Add album section

--- tutorial/music/urls.py
+++ tutorial/music/urls.py
@@ -5,11 +5,10 @@ app_name='music'
 
 urlpatterns = [
     #music
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    #music /<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
-    # music /<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)favorite/$', views.favorite, name='favorite'),
+    #/music/album/add/ crea nuevo album con su primary key
+    url(r'album/add/$', views.AlbumCreate.as_view() , name='album-add'),
 ]
